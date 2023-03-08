93b9499b66f6af7ed1f3e1d08fa9f5e98a590d4f
akawincent <3511606256@qq.com>
2023-03-08 21:08:58 +0800

    update ImmaturePoint.h

--- FullSystem/ImmaturePoint.h
+++ FullSystem/ImmaturePoint.h
@@ -44,11 +44,17 @@ public:
 
 //点的状态
 enum ImmaturePointStatus {
+	//表示最近一次在极线上进行成功匹配
 	IPS_GOOD=0,					// traced well and good
+	//表示追踪结束，或者是追踪的点在投用时已经到图像外面了
 	IPS_OOB,					// OOB: end tracking & marginalize!
+	//表示追踪失败
 	IPS_OUTLIER,				// energy too high: if happens again: outlier!
+	//表示虽然成功追踪 但是视差太小(不利于深度滤波) 可以跳过
 	IPS_SKIPPED,				// traced well and good (but not actually traced).
+	//表示因为一些坏条件导致追踪失败
 	IPS_BADCONDITION,			// not traced because of bad condition.
+	//表示还没有进行过深度追踪
 	IPS_UNINITIALIZED			// not even traced once.
 };			
 
@@ -59,7 +65,9 @@ public:
 	EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
 	//static values
 	//MAX_RES_PER_POINT = 8
+	//存储该像素邻域8个点的像素值
 	float color[MAX_RES_PER_POINT];
+	//邻域8个像素点的权重wp
 	float weights[MAX_RES_PER_POINT];
 	
 	//点以及其邻域点的梯度平方和
@@ -69,12 +77,12 @@ public:
 	float energyTH;
 	//像素坐标
 	float u,v;
-	//论文中的主导帧
+	//点的host frame
 	FrameHessian* host;
 	int idxInImmaturePoints;
-
+	//特征点质量
 	float quality;
-
+	//选点时的种类
 	float my_type;
 
 	//逆深度的上下限度
@@ -86,9 +94,9 @@ public:
 
 	ImmaturePointStatus traceOn(FrameHessian* frame, const Mat33f &hostToFrame_KRKi, const Vec3f &hostToFrame_Kt, const Vec2f &hostToFrame_affine, CalibHessian* HCalib, bool debugPrint=false);
 
-	ImmaturePointStatus lastTraceStatus; 	//最新极限跟踪的点的状态
-	Vec2f lastTraceUV;						//最新极限跟踪的点的像素坐标
-	float lastTracePixelInterval;
+	ImmaturePointStatus lastTraceStatus; 	//上一次极限跟踪的点的状态
+	Vec2f lastTraceUV;						//上一次限跟踪的点的像素坐标
+	float lastTracePixelInterval;			//上一次极限跟踪的视差lamda
 
 	float idepth_GT;
 
